6f167dcab0d23483f932f73c58876cf9a5da2f36
yue.zhong <yue.zhong@patpat.com>
2020-06-23 12:10:41 +0800

    second commit

---
+++ js/member.js
@@ -0,0 +1,170 @@
+var member = [
+  {
+    "phone": "No.1",
+    "name": "冯家浩"
+  },
+  {
+    "phone": "No.2",
+    "name": "田能"
+  },
+  {
+    "phone": "No.3",
+    "name": "许锐"
+  },
+  {
+    "phone": "No.4",
+    "name": "游利涛"
+  },
+  {
+    "phone": "No.5",
+    "name": "钟福明"
+  },
+  {
+    "phone": "No.6",
+    "name": "谢易涓"
+  },
+  {
+    "phone": "No.7",
+    "name": "潘蕾"
+  },
+  {
+    "phone": "No.8",
+    "name": "龙驰"
+  },
+  {
+    "phone": "No.9",
+    "name": "周煜皓"
+  },
+  {
+    "phone": "No.10",
+    "name": "郑汉荣"
+  },
+  {
+    "phone": "No.11",
+    "name": "成建锋"
+  },
+  {
+    "phone": "No.12",
+    "name": "阳憶松"
+  },
+  {
+    "phone": "No.13",
+    "name": "王勤伟"
+  },
+  {
+    "phone": "No.14",
+    "name": "王明俭"
+  },
+  {
+    "phone": "No.15",
+    "name": "龙丽红"
+  },
+  {
+    "phone": "No.16",
+    "name": "黄淦"
+  },
+  {
+    "phone": "No.17",
+    "name": "张洪亮"
+  },
+  {
+    "phone": "No.18",
+    "name": "李树健"
+  },
+  {
+    "phone": "No.19",
+    "name": "孙玲"
+  },
+  {
+    "phone": "No.20",
+    "name": "许琳华"
+  },
+  {
+    "phone": "No.21",
+    "name": "伍黎佳"
+  },
+  {
+    "phone": "No.22",
+    "name": "敖雪"
+  },
+  {
+    "phone": "No.23",
+    "name": "刘伟鸿"
+  },
+  {
+    "phone": "No.24",
+    "name": "魏炫平"
+  },
+  {
+    "phone": "No.25",
+    "name": "秧艳银"
+  },
+  {
+    "phone": "No.26",
+    "name": "黄群"
+  },
+  {
+    "phone": "No.27",
+    "name": "陈正云"
+  },
+  {
+    "phone": "No.28",
+    "name": "尹锋"
+  },
+  {
+    "phone": "No.29",
+    "name": "罗回归"
+  },
+  {
+    "phone": "No.30",
+    "name": "何元"
+  },
+  {
+    "phone": "No.31",
+    "name": "钟岳"
+  },
+  {
+    "phone": "No.32",
+    "name": "温劲怀"
+  },
+  {
+    "phone": "No.33",
+    "name": "胡勇"
+  },
+  {
+    "phone": "No.34",
+    "name": "麦可文"
+  },
+  {
+    "phone": "No.35",
+    "name": "杨青"
+  },
+  {
+    "phone": "No.36",
+    "name": "南岳阳"
+  },
+  {
+    "phone": "No.37",
+    "name": "郑天"
+  },
+  {
+    "phone": "No.38",
+    "name": "李昵"
+  },
+  {
+    "phone": "No.39",
+    "name": "郑定正"
+  },
+  {
+    "phone": "No.40",
+    "name": "胡萌"
+  },
+  {
+    "phone": "No.41",
+    "name": "杨文学"
+  },
+  {
+    "phone": "No.42",
+    "name": "刘帝华"
+  }
+]
